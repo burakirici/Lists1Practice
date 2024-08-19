5f2a710ef65b2ad241151e4a81ecc6a49de3bcb8
burak <burak@BURAK>
2024-08-19 11:15:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> guests = new List<string>();
+
+        guests.Add("Bülent Ersoy");
+
+        guests.Add("Ajda Pekkan");
+
+        guests.Add("Ebru Gündeş");
+
+        guests.Add("Hadise");
+
+        guests.Add("Hande Yener");
+
+        guests.Add("Tarkan");
+
+        guests.Add("Funda Arar");
+
+        guests.Add("Demet Akalın");
+
+        Console.WriteLine("** Guests **");
+        int sort = 1;
+
+        foreach (string s in guests)
+        {
+            
+            Console.WriteLine($"{sort} -  {s}");
+            sort++;
+        }
+
+
+
+    }
+}
